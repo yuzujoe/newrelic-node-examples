fcf485d9a647f0837cc62c0662f11f7de059b374
yuzujoe <yuzun225@gmail.com>
2025-04-27 18:57:19 +0900

    use nrExternals package for turbopack config

--- nextjs/nextjs-turbopack/next.config.js
+++ nextjs/nextjs-turbopack/next.config.js
@@ -1,5 +1,12 @@
 'use strict'
 
+const nrExternals = require('newrelic/load-externals')
+
 module.exports = {
   serverExternalPackages: ['newrelic'],
+
+  torbopack: (config) => {
+    nrExternals(config)
+    return config
+  }
 }
